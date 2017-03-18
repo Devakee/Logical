697f52a658e8158808bd201c03762a44d7f6ce80
Devakee Rathour <Devakee Rathour@Devakee>
2017-03-19 01:09:57 +0530

    TestClass

---
+++ Logical/src/com/devakee/array/TestClass1.java
@@ -0,0 +1,19 @@
+package com.devakee.array;
+
+public class TestClass1 {
+	
+	public static void main(String[] args) {
+		printNumber();
+		
+	}
+	
+	public static void printNumber(){
+		
+		for(int i=1; i<=10; i++)
+		{
+			System.out.println(i);
+		}
+	}
+	
+
+}
